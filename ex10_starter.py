4ee852c8dbcb9c743b662754120673880ad7e437
Nadine Lathifah <nadinehendronoto@gmail.com>
2025-01-26 22:22:29 +0300

    EDIT: Exercise 10 Question 2

--- ex10_starter.py
+++ ex10_starter.py
@@ -61,3 +61,33 @@ for file in glob.glob(pattern):
     filename = os.path.basename(file)
     print(filename)
 
+# Question 2:
+password = "1234"
+# The correct PIN code is stored as a string in the variable password.
+
+attempt=1
+# Define a variable to store the number of attempts.
+
+supplied_pin = input("Enter your PIN: ")
+# The input function prompts the user to give a password and stores it as a string in the supplied_pin variable.
+
+# Use a while loop to prompt the user for input while the number of attempts is less than 3.
+while attempt <4:
+    # Using an if statement, we can check if the supplied pin equals the correct password.
+    # if it does, then we print a suitable message and break out of the while loop.
+    if supplied_pin == password:
+        print("You can enjoy freedom for another day!")
+        # "You may now enter the amount you would like to withdraw."
+        break
+        # If the attempts is between 1 and 3, ask the user to try again.
+    elif 1<=attempt<3:
+        print("Wrong password. Please try again if you don't want to go to jail.")
+        print("Attempt #"+str(attempt)+" out of #3")
+        # We increment (increase the size) the number of attempts.
+        attempt += 1
+        supplied_pin = input("Enter your PIN: ")
+    # If the number of attempts is more than 3, then print a suitable message and exit the while loop.
+    else:
+        print("Police are coming for you!")
+        # additional comment: "Your account has been locked."
+        break
